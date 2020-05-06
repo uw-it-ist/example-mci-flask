66278d279d3ff58e4deb8fa2b9215c2c00bcc0e5
Benjamin Roy <benroy@uw.edu>
2020-05-05 23:47:04 -0700

    Include unit tests
    Full unit test coverage should be part of a good example project.

---
+++ tests/test_flask.py
@@ -0,0 +1,75 @@
+from unittest.mock import patch
+
+import os
+os.environ['FLASK_SESSION_KEY'] = 'abc123'  # noqa: E402
+os.environ['DSN_TOOLSDB'] = 'host=localhost user=toolop dbname=ripit_2 sslmode=require'  # noqa: E402
+
+# monkey patch flask_caching to work without redis
+from flask_caching import Cache
+import flask_app.app
+flask_app.app.cache = Cache(config={
+    "CACHE_TYPE": "simple",
+})
+from flask_app.loader import app  # noqa: E402
+
+
+def test_health_check():
+    with app.test_client() as c:
+        rv = c.get(path='/healthz')
+        assert b'OK' in rv.data and rv.status_code == 200
+
+
+def test_api_health_check():
+    with app.test_client() as c:
+        rv = c.get(path='/api/healthz')
+        assert b'{"pass":true}' in rv.data and rv.status_code == 200
+
+
+def test_index_page_401():
+    with app.test_client() as c:
+        rv = c.get(path='/')
+        assert b'You do not have access' in rv.data and rv.status_code == 401
+
+
+def test_item_page_401():
+    with app.test_client() as c:
+        rv = c.get(path='/item/1')
+        assert b'You do not have access' in rv.data and rv.status_code == 401
+
+
+def test_item_page():
+    app.testing = True
+    test_auth_header = {'X-Forwarded-User': 'test-user'}
+    with app.test_client() as c:
+        rv = c.get(headers=test_auth_header, path='/item/1')
+        assert b'This is item id' in rv.data and rv.status_code == 200
+
+
+def test_db_down():
+    app.testing = True
+    test_auth_header = {'X-Forwarded-User': 'test-user'}
+    with app.test_client() as c:
+        rv = c.get(headers=test_auth_header, path='/')
+        assert b'Unable to connect to database' in rv.data and rv.status_code == 500
+
+
+@patch("psycopg2.connect")
+def test_index_page(mock_connect):
+    expected = [{'id': 1, 'name': 'foo'}, {'id': 2, 'name': 'bar'}]
+    mock_connect.return_value.cursor.return_value.fetchall.return_value = expected
+    app.testing = True
+    test_auth_header = {'X-Forwarded-User': 'test-user'}
+    with app.test_client() as c:
+        rv = c.get(headers=test_auth_header, path='/')
+        assert b'core-content-container' in rv.data and rv.status_code == 200
+
+
+def test_saml_groups():
+    app.testing = True
+    test_auth_header = {
+        'X-Forwarded-User': 'test-user',
+        'X-Forwarded-Groups': 'group1:group2'
+        }
+    with app.test_client() as c:
+        rv = c.get(headers=test_auth_header, path='/item/1')
+        assert b'This is item id' in rv.data and rv.status_code == 200
